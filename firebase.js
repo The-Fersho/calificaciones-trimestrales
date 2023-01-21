8c74c5cec3bff5b90ada17733aca97f430761e9b
Fernando Rubio <est.cfcalderonr0201@uml.edu.ni>
2023-01-21 15:02:56 -0600

    Instalacion de firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmrY7A5Abp8lQIoUW13J3tbztAAKM9cI8",
+  authDomain: "calificaciones-cb40c.firebaseapp.com",
+  projectId: "calificaciones-cb40c",
+  storageBucket: "calificaciones-cb40c.appspot.com",
+  messagingSenderId: "865541555327",
+  appId: "1:865541555327:web:6d9116396c879c0f3aab4a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
