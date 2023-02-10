ab5de6053a56fa059529f1143f9f22908b8c3bf0
Fernando Rubio <est.cfcalderonr0201@uml.edu.ni>
2023-02-10 17:49:42 -0600

    Completado el desarrollo de la aplicacion basica

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from "vite";
+const { resolve } = require('path')
+//Add env variable to check if is production or development
+
+const env = process.env;
+//Set a base route if deploying on github pages
+env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode);
 
 export default defineConfig({
   root: "./",
-  base: "/",
+  base: env.mode === "production" ? "/calificaciones-trimestrales/" : "/",
   build: {
     outDir: "dist",
-    assetsDir: "assets"
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        trimestres: resolve(__dirname, 'trimestres.html'),
+        login: resolve(__dirname, 'login.html'),
+        calificaciones: resolve(__dirname, 'calificaciones.html')
+      }
+    }
   },
 
   server: {
