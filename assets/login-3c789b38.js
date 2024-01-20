c98f9fe18daee0d4f4aaa5caa67d85d7bf13aa45
The-Fersho <The-Fersho@users.noreply.github.com>
2024-01-20 15:40:27 +0000

    deploy: 7600c13ff87e6fe017af1b753c7e372cd13cb6d1

--- assets/login-3c789b38.js
+++
@@ -1,1 +0,0 @@
-import{l as o}from"./auth-5df674f1.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
---
+++ assets/login-9624979c.js
@@ -0,0 +1,1 @@
+import{l as o}from"./auth-660a9e46.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
